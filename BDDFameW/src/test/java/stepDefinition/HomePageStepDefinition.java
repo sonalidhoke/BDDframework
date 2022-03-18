6fef657d1d9b17bd89d62fcb7f3eb17f7b5cd67a
Pranali Meshram <pranalimeshram246@gmail.com>
2022-03-18 10:31:41 +0530

    homePagestepdefinition commiting to NewBranch

---
+++ BDDFameW/src/test/java/stepDefinition/HomePageStepDefinition.java
@@ -0,0 +1,11 @@
+package stepDefinition;
+
+import org.junit.Test;
+
+public class HomePageStepDefinition {
+//i havnt write anything here because i m just checking 
+	@Test
+	public void check() {
+		System.out.println("checking");
+	}
+}
